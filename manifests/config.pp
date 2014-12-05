b53959196ea0e81cf398a9578fe4571264e1712c
takumi <takumi@tmfam.com>
2014-12-06 00:25:51 +0900

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,7 +10,7 @@ class php_fpm::config {
     group        => 0,
     mode         => '0644',
     content      => template($::php_fpm::config_template),
-    validate_cmd => "$::php_fpm::prefix/sbin/php-fpm -y -c %",
+    validate_cmd => "$::php_fpm::prefix/sbin/php-fpm -t -y %",
   }
 
   file { "$::php_fpm::include":
